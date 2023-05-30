ac08b47e1aa73df85ed691639e82f7319de57dbf
omargalal232 <omargalal232@gmail.com>
2023-05-30 21:40:52 +0300

    Modifying admindashboard and routes

---
+++ public/js/dashboard.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Activate feather-icons
+    feather.replace();
+});
--- routes/admindashboard.js
+++ routes/admindashboard.js
@@ -6,4 +6,29 @@ router.get("/", (req, res, next) => {
     res.render("admindashboard");
 });
 
+// GET : /admin_addAdmin
+router.get("/", (req, res, next) => {
+    res.render("admin_addAdmin");
+});
+
+// GET : /admin_customers
+router.get("/", (req, res, next) => {
+    res.render("admin_customers");
+});
+
+// GET : /admin_dashboard
+router.get("/", (req, res, next) => {
+    res.render("admin_dashboard");
+});
+
+// GET : /admin_reports
+router.get("/", (req, res, next) => {
+    res.render("admin_reports");
+});
+
+// GET : /admin_tours
+router.get("/", (req, res, next) => {
+    res.render("admin_tours");
+});
+
 module.exports = router;
