1740df67557db5d6f1944d8d52d7169a48831f72
kamolia <125690872+kamolia1@users.noreply.github.com>
2023-06-03 17:18:43 +0300

    Update flight.js
    update in the function that retrive the database

--- controllers/flight.js
+++ controllers/flight.js
@@ -1,15 +1,15 @@
 const flight = require("../models/flight");
 
 const getflight = (req, res) => {
-  flight.find()
-    .then((result) => {
-      res.render("flight", { flight: result, users: req.session.users || null });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.render("flight", { flight: [], users: req.session.users || null });
-    });
-};
+    flight.find()
+      .then((result) => {
+        res.render("flight", { flights: result, users: req.session.users || null });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.render("flight", { flights: [], users: req.session.users || null });
+      });
+  };
 
 module.exports = {
   getflight: getflight
