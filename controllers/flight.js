e77d07a0bcca348c4f77f7d006853fb3e8b6344b
kamolia <125690872+kamolia1@users.noreply.github.com>
2023-06-03 17:50:22 +0300

    Update flight.js
    update in controller

--- controllers/flight.js
+++ controllers/flight.js
@@ -1,16 +1,35 @@
 const flight = require("../models/flight");
 
 const getflight = (req, res) => {
-    flight.find()
-      .then((result) => {
-        res.render("flight", { flights: result, users: req.session.users || null });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.render("flight", { flights: [], users: req.session.users || null });
-      });
+    const { searchQuery } = req.query;
+  
+    if (searchQuery) {
+      // Perform a case-insensitive search for flights matching the search query
+      flight.find({ $text: { $search: searchQuery, $caseSensitive: false } })
+        .then((result) => {
+          if (result.length === 0) {
+            // No flights found for the search query
+            res.render("flight", { flights: [], users: req.session.users || null, searchQuery });
+          } else {
+            res.render("flight", { flights: result, users: req.session.users || null, searchQuery });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          res.render("flight", { flights: [], users: req.session.users || null, searchQuery });
+        });
+    } else {
+      // No search query provided, render all flights
+      flight.find()
+        .then((result) => {
+          res.render("flight", { flights: result, users: req.session.users || null, searchQuery });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.render("flight", { flights: [], users: req.session.users || null, searchQuery });
+        });
+    }
   };
-
 module.exports = {
   getflight: getflight
 };
