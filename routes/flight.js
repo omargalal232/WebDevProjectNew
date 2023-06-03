5ae9d17b1d7af06716ff5e093ceb0905ac809d02
kamolia <125690872+kamolia1@users.noreply.github.com>
2023-06-03 17:16:50 +0300

    Create flight.js
    create routes of the flight page

---
+++ routes/flight.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const flight = require("../controllers/flight");
+
+  router.get("/flight", (req, res) => {
+  
+    res.render("flight",{ users: req.session.users || null });
+
+});
+
+
+
+router.get("/flight", (req, res) => {
+  res.render("flight");
+
+});
+
+
+
+router.get("/get", flight.getflight);
+
+
+module.exports = router;
