eb39dc121192df623efc32fcd1beb71d20d6a7eb
kamolia <125690872+kamolia1@users.noreply.github.com>
2023-06-03 17:45:02 +0300

    Update flight.js
    update in routes

--- routes/flight.js
+++ routes/flight.js
@@ -2,22 +2,15 @@ const express = require("express");
 const router = express.Router();
 const flight = require("../controllers/flight");
 
-  router.get("/flight", (req, res) => {
-  
-    res.render("flight",{ users: req.session.users || null });
-
-});
-
-
-
 router.get("/flight", (req, res) => {
-  res.render("flight");
-
-});
-
+    res.render("flight", { users: req.session.users || null });
+  });
 
 
-router.get("/get", flight.getflight);
+
+
+
+router.get("/get", hotels.gethotel);
 
 
 module.exports = router;
