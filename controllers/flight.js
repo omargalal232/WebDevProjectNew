8da450b31929795ee92344d3360cfc759fdcb215
kamolia <125690872+kamolia1@users.noreply.github.com>
2023-06-04 16:48:53 +0300

    finallyyyyy the flight

--- controllers/flight.js
+++ controllers/flight.js
@@ -1,19 +1,39 @@
 const Flight = require('../models/flight');
-
 const getflight = (req, res, next) => {
-  const { from, to,price } = req.body;
+  const { from, to, price } = req.body;
 
-  Flight.findOne({ from:from, to:to,price:price })
-     .then(result => {
-    console.log(result);
+  Flight.find({ from, to, price })
+    .then((result) => {
+      res.render("flight", {
+        objflight: result,
+        users: req.session.users || null,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("flight", {
+        objflight: [],
+        users: req.session.users || null,
+        errorMessage: "An error occurred. Please try again.",
+      });
+    });
+};
 
-     })
-     .catch(err => {
-        console.log(err);
-        res.render("flight", { errorMessage: "An error occurred. Please try again." });
-     });
+
+
+
+const flightdetailes = (req, res) => {
+  Flight.findById(req.params.id)
+    .then((result) => {
+      res.render("flight", { objflight: result, users: req.session.users || null });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("flight", { objflight: [], users: req.session.users || null });
+    });
 };
 
 module.exports = {
-  getflight:getflight
+  getflight:getflight,
+  flightdetailes:flightdetailes
 };
--- routes/flight.js
+++ routes/flight.js
@@ -1,11 +1,28 @@
+
+
+
 const express = require("express");
 const router = express.Router();
-const flightController = require("../controllers/flight");
+const Flight = require("../controllers/flight");
 
-router.get("/get", (req, res) => {
-  res.render("flight", { users: req.session.users || null });
+  router.get("/flight", (req, res) => {
+  
+    res.render("flight",{ users: req.session.users || null });
+
 });
 
 
-router.post("/search",flightController.getflight);
+
+
+
+
+
+
+
+
+
+
+router.post("/get", Flight.getflight);
+router.get("/get",Flight.flightdetailes);
+router.get("/get/:id",Flight.flightdetailes); 
 module.exports = router;
