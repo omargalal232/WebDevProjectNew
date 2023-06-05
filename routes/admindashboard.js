1020bd0493186e304506eb7b6d0ba70fe1a89724
Ahmed21gamal <125803560+Ahmed21gamal@users.noreply.github.com>
2023-06-05 04:43:44 +0300

    add some features for admin_hotel like retrieve the data of the hotels in table and making some modifying in files of admin dashboard

--- routes/admindashboard.js
+++ routes/admindashboard.js
@@ -1,47 +1,55 @@
 const express = require("express");
 const router = express.Router();
+const hotels = require("../models/hotels");
 
 // GET : /admindashboard
 router.get("/", (req, res) => {
-    
-    res.render("admindashboard",{ users: req.session.users || null });
+
+    res.render("admindashboard", { users: req.session.users || null });
 });
 
 router.get("/admindashboard", (req, res) => {
-    
+
     res.render("admindashboard", { users: req.session.users || null });
 });
 
 // GET : /admin_dashboard
 router.get("/admin_dashboard", (req, res) => {
-    
-    res.render("admin_dashboard",{ users: req.session.users || null });
+
+    res.render("admin_dashboard", { users: req.session.users || null });
 });
 
 // GET : /admin_addAdmin
 router.get("/admin_addAdmin", (req, res) => {
-   
-    res.render("admin_addAdmin",{ users: req.session.users || null });
+
+    res.render("admin_addAdmin", { users: req.session.users || null });
 });
 
 // GET : /admin_customers
 router.get("/admin_customers", (req, res) => {
-    
-    res.render("admin_customers",{ users: req.session.users || null });
-});
 
-// GET : /admin_reports
-router.get("/admin_reports", (req, res) => {
-   
-    res.render("admin_reports", { users: req.session.users || null });
+    res.render("admin_customers", { users: req.session.users || null });
 });
 
 // GET : /admin_tours
 router.get("/admin_tours", (req, res) => {
-   
-    res.render("admin_tours",{ users: req.session.users || null });
+
+    res.render("admin_tours", { users: req.session.users || null });
 });
 
+router.get("/admin_cruise", (req, res) => {
 
+    res.render("admin_cruise", { users: req.session.users || null });
+});
+
+router.get("/admin_hotel", (req, res) => {
+    hotels.find()
+        .then(result => {
+            res.render("admin_hotel", {hotels:result, users: req.session.users || null  });
+        })
+        .catch(err => {
+            console.log(err)
+        })
+});
 
 module.exports = router;
