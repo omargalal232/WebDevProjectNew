84ffdcbd9c5bac8c2aabc4d5dfe40075f9c20b83
Yousef-Hany <yousefkhattab610@gmail.com>
2023-06-06 18:23:00 +0300

    update to tourdetails

--- models/cairotours.js
+++ models/cairotours.js
@@ -25,15 +25,15 @@ const cairotoursSchema = new mongoose.Schema({
     type: Number,
     required: true
   },
-image:{
+img1:{
 type: String,
 required:true
 },
-image2:{
+img2:{
   type: String,
   required:true
   },
-  image3:{
+  img3:{
     type: String,
     required:true
     }
@@ -51,9 +51,9 @@ const cairotours = mongoose.model('cairotours', cairotoursSchema);
 //       price: 550,
 //       description: 'our tour manager from Memphis Tours will pick you up from your hotel in Cairo, starting your Pyramid Tours visiting the Pyramids of Cheops, Chephren and Mykerinus. Then proceed to visit the Great Sphinx, the head of a pharaoh with a lions body, dates from the time of Chephren',
 //       ticketsavailable: 5,
-//       image:'https://serving.photos.photobox.com/26174230e94a4b8f3ff5ccecf70e690969222b5edd293c1ab97c7f2cb1f6dcfacc2970a6.jpg',
-//       image2:'https://serving.photos.photobox.com/26174230e94a4b8f3ff5ccecf70e690969222b5edd293c1ab97c7f2cb1f6dcfacc2970a6.jpg',
-//       image3:'https://serving.photos.photobox.com/26174230e94a4b8f3ff5ccecf70e690969222b5edd293c1ab97c7f2cb1f6dcfacc2970a6.jpg'
+//       img1:'https://serving.photos.photobox.com/906296120b328dc6c8051dacb11625528da35375a28bcdb47f82b405e41fc168eb8aad1e.jpg',
+//       img2:'https://serving.photos.photobox.com/38422983394cfdaa7b580844aa32db1295c5622f71d51aea7100eb80ffe704b032f8f359.jpg',
+//       img3:'https://serving.photos.photobox.com/49477926628cffb9bbf71257bca8448be1eb69dbda4b84cecdf1a97891e64a4e2893c9dd.jpg'
 // });
 // cai.save();
 
