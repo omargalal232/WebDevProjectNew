b58f06792597e98fad5407684a1a53fc2823a0be
omargalal232 <omargalal232@gmail.com>
2023-06-06 19:17:55 +0300

    Created a new route for adding tours and hotels to the wishlist.

---
+++ routes/wishlist.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const city = require('../models/city');
+const hotels = require('../models/hotel');
+
+router.post("/add", async (req, res) => {
+    const { type, itemId } = req.body;
+    const userId = req.session.users._id;
+
+    if (type === "tour") {
+        await city.findByIdAndUpdate(itemId, { $addToSet: { usersWithWishlist: userId } });
+    } else if (type === "hotel") {
+        await hotels.findByIdAndUpdate(itemId, { $addToSet: { usersWithWishlist: userId } });
+    }
+
+    res.json({ success: true });
+});
+
+module.exports = router;
